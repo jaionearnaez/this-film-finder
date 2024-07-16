e2061f8130b6bc192ac038924f32d4d99917bbbb
jaio_arte <arte.jaio@gmail.com>
2024-07-16 19:40:56 +0200

    app working with ionic

--- apps/this-film-finder/src/app/app.component.ts
+++ apps/this-film-finder/src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { IonButton, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [IonRouterOutlet, IonButton],
   selector: 'this-film-finder-root',
-  templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: ` <ion-router-outlet></ion-router-outlet>
+    <ion-button (click)="showAlert()">it works!</ion-button>`,
 })
 export class AppComponent {
   title = 'this-film-finder';
+  showAlert(){
+    alert('it works!')
+  }
 }
