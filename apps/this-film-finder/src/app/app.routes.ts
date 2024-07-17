2595cfcddedad26dc61fa9dc6a6969abf5e18134
jaio_arte <arte.jaio@gmail.com>
2024-07-18 01:24:43 +0200

    create initial feature-login

--- apps/this-film-finder/src/app/app.routes.ts
+++ apps/this-film-finder/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'login',
+    title: 'This Film Finder: login',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import(
+        '@this-film-finder/feature-login/components/login.component'
+      ).then((m) => m.LoginComponent),
+    data: {
+      showSideMenu: true,
+      showHeader: true,
+      showFooter: true,
+      menu: {
+        title: 'Login',
+      },
+    },
+  },
+];
