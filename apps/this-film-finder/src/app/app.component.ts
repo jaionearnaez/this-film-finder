538f91d91fbaa257e54ed833c3b235ed7654454a
jaio_arte <arte.jaio@gmail.com>
2024-07-18 01:29:16 +0200

    initial app component html and routes info

--- apps/this-film-finder/src/app/app.component.ts
+++ apps/this-film-finder/src/app/app.component.ts
@@ -1,18 +1,39 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { IonButton, IonRouterOutlet } from '@ionic/angular/standalone';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  IonApp,
+  IonButton,
+  IonContent,
+  IonRouterOutlet,
+} from '@ionic/angular/standalone';
+import { Store } from '@ngrx/store';
+import { selectShowFooter, selectShowHeader, selectUrl } from './selectors/router.selectors';
 
 @Component({
   standalone: true,
-  imports: [IonRouterOutlet, IonButton],
+  imports: [IonRouterOutlet, IonButton, IonApp, IonContent],
   selector: 'this-film-finder-root',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: ` <ion-router-outlet></ion-router-outlet>
-    <ion-button (click)="showAlert()">it works!</ion-button>`,
+  template: `
+    <ion-app>
+      @if(showHeader()){
+      <p>header1 here</p>
+      }
+      <ion-content>
+        <ion-router-outlet></ion-router-outlet>
+      </ion-content>
+      @if(showFooter()){
+      <p>footer1 here</p>
+      }
+    </ion-app>
+  `,
 })
 export class AppComponent {
+  private readonly store = inject(Store);
   title = 'this-film-finder';
-  showAlert(){
-    alert('it works!')
-  }
+
+  currentUrl = this.store.selectSignal(selectUrl);
+  // isLoggedIn = this.store.selectSignal(AuthFeatureState.selectIsLoggedIn);
+  showHeader = this.store.selectSignal(selectShowHeader);
+  showFooter = this.store.selectSignal(selectShowFooter);
 }
