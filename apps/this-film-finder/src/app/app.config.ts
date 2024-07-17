9c38fa5689defe1d061be940ec0d15844833bc92
jaio_arte <arte.jaio@gmail.com>
2024-07-18 01:26:27 +0200

    store logger and router selectors

--- apps/this-film-finder/src/app/app.config.ts
+++ apps/this-film-finder/src/app/app.config.ts
@@ -27,6 +27,7 @@ import { authStoreProvider } from '@this-film-finder/feature-auth/feature-auth.p
 import { filter, take } from 'rxjs';
 import { environment } from '../environments/environment';
 import { appRoutes } from './app.routes';
+import * as LogEffects from './store/log.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -48,8 +49,10 @@ export const appConfig: ApplicationConfig = {
     }),
     provideStore({ router: routerReducer }),
     provideRouterStore(),
+    provideEffects(LogEffects),
     ...authStoreProvider,
 
+    environment.providers,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     { provide: MOVIES_API_BASE_URL, useValue: environment.moviesApiBaseUrl },
 
---
+++ apps/this-film-finder/src/app/selectors/router.selectors.ts
@@ -0,0 +1,33 @@
+import { getRouterSelectors } from '@ngrx/router-store';
+import { createSelector } from '@ngrx/store';
+// `router` is used as the default feature name. You can use the feature name
+// of your choice by creating a feature selector and pass it to the `getSelectors` function
+// export const selectRouter = createFeatureSelector<RouterReducerState>('yourFeatureName');
+export const {
+  selectCurrentRoute, // select the current route
+  //   selectFragment, // select the current route fragment
+  //   selectQueryParams, // select the current route query params
+  //   selectQueryParam, // factory function to select a query param
+  //   selectRouteParams, // select the current route params
+  //   selectRouteParam, // factory function to select a route param
+  selectRouteData, // select the current route dataI
+  selectUrl, // select the current url
+  selectTitle: selectRouterTitle, // Select the title if available
+} = getRouterSelectors();
+
+// export const selectShowSideMenu = createSelector(
+//   selectRouteData,
+//   (data) => data?.['showSideMenu'],
+// );
+// export const selectIsSettingEnabled = createSelector(
+//   selectRouteData,
+//   (data) => data?.['isSettingEnabled'],
+// );
+
+export const selectShowHeader = createSelector(selectRouteData, (data) =>
+  data?.['showHeader'] ? data?.['showHeader'] : false
+);
+
+export const selectShowFooter = createSelector(selectRouteData, (data) =>
+  data?.['showFooter'] ? data?.['showFooter'] : false
+);
