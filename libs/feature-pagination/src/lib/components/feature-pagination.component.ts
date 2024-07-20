38aabcf4719e4aa3c39a196e14c79f688aed34a0
jaio_arte <arte.jaio@gmail.com>
2024-07-21 01:04:23 +0200

    pagination component working

--- libs/feature-pagination/src/lib/components/feature-pagination.component.ts
+++ libs/feature-pagination/src/lib/components/feature-pagination.component.ts
@@ -1,12 +1,127 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  output,
+} from '@angular/core';
+import {
+  IonButton,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+} from '@ionic/angular/standalone';
 
 @Component({
-  selector: 'this-film-finder-pagination-feature-pagination',
+  selector: 'this-film-finder-pagination',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>feature-pagination works!</p>`,
+  imports: [IonButton, IonIcon, IonItem, IonLabel, IonInput],
+  template: `
+    @if (currentPage() &&totalPages()) {
+    <div class="pagination">
+      <div class="paginationButtons">
+        <ion-button
+          fill="clear"
+          (click)="doGoToPage(currentPage(), -1)"
+          [disabled]="currentPage() <= 1"
+        >
+          <ion-icon name="chevron-back-outline"></ion-icon>
+        </ion-button>
+
+        @for (eachPageNumber of pagesToShow(); track $index) { @if ( $index > 0
+        && pagesToShow()[$index] - pagesToShow()[$index - 1] > 1 ) {
+        <ion-button class="full-color" disabled fill="clear">...</ion-button>
+        }
+        <ion-button
+          class="full-color"
+          (click)="goToPage.emit(eachPageNumber)"
+          fill="clear"
+          [style.border]="
+            currentPage() === eachPageNumber
+              ? '2px var(--ion-color-secondary) solid'
+              : ''
+          "
+          [disabled]="currentPage() === eachPageNumber"
+          >{{ eachPageNumber }}</ion-button
+        >
+        }
+
+        <ion-button
+          [disabled]="currentPage() >= totalPages()"
+          fill="clear"
+          (click)="doGoToPage(currentPage(), +1)"
+
+        >
+          <ion-icon name="chevron-forward-outline"></ion-icon>
+        </ion-button>
+      </div>
+      <div class="paginationGoto">
+        <ion-item lines="none" [style.border-right]="'1px #D1CBCF solid'">
+          <ion-label>
+            <h3>Page Count</h3>
+            <p>{{ totalPages() }}</p></ion-label
+          >
+        </ion-item>
+        <ion-item>
+          <ion-input
+            class="goToPage"
+            fill="solid"
+            labelPlacement="stacked"
+            label="Go to page"
+            type="number"
+            #gotoPageNumber
+            (keyup.enter)="doGoToPage(gotoPageNumber.value ?? 1)"
+            [value]="currentPage()"
+            enterkeyhint="go"
+          >
+          </ion-input>
+        </ion-item>
+
+        <ion-item>
+          <ion-input
+            class="setPageSize"
+            fill="solid"
+            labelPlacement="stacked"
+            label="Films per page"
+            inputmode="numeric"
+            #setPageSizeNumber
+            (keyup.enter)="doChangePageSize(setPageSizeNumber.value ?? 25)"
+            [value]="limit()"
+            enterkeyhint="go"
+          ></ion-input>
+        </ion-item>
+      </div>
+    </div>
+    }
+  `,
   styleUrl: './feature-pagination.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FeaturePaginationComponent {}
+export class FeaturePaginationComponent {
+  currentPage = input.required<number>();
+  totalPages = input.required<number>();
+  limit = input.required<number>();
+
+  pagesToShow = input<Array<number>>([]);
+
+  readonly goToPage = output<number>();
+  readonly changePageSize = output<number>();
+
+  doGoToPage(_pageNumber: number | string, diff?:number) {
+    const pageNumber: number =
+      typeof _pageNumber === 'number' ? _pageNumber : Number(_pageNumber);
+
+    if (pageNumber) {
+      this.goToPage.emit(pageNumber+(diff?diff:0));
+    }
+  }
+
+  doChangePageSize(_pageSize: number | string) {
+    const pageSize: number =
+      typeof _pageSize === 'number' ? _pageSize : Number(_pageSize);
+
+    if (pageSize) {
+      this.changePageSize.emit(pageSize);
+    }
+  }
+}
