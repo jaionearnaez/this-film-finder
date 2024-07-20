4e0d2c6096d6b2c4a69e7a26a053759c355343ee
jaio_arte <arte.jaio@gmail.com>
2024-07-20 20:42:06 +0200

    api-not-contentful layout

--- apps/this-film-finder/src/app/components/api-not-contenful.component.ts
+++ apps/this-film-finder/src/app/components/api-not-contenful.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
+import { IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'this-film-finder-api-not-contenful',
   standalone: true,
-  imports: [],
+  imports: [IonButton],
   template: `
-    <p>
-      api-not-contenful 
-    </p>
-    <p>
-      please try again later
-    </p>
+    <img src="assets/broken-egg.svg" />
+    <p>Ooops! we are not ready</p>
+    <p>Please try again later</p>
+    <!-- <ion-button (click)="location.back()">Try again</ion-button> -->
   `,
-  styleUrl: './api-not-contenful.component.scss'
+  styleUrl: './api-not-contenful.component.scss',
 })
 export class ApiNotContenfulComponent {
-
 }
