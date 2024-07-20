77a2e7ce94d7ab729aacdd6654623692ae4d1047
jaio_arte <arte.jaio@gmail.com>
2024-07-20 20:42:57 +0200

    new header with theme

---
+++ apps/this-film-finder/src/app/components/header.component.ts
@@ -0,0 +1,28 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IonHeader, IonItem, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'this-film-finder-header',
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonItem, RouterLink],
+  template: `
+    <ion-header>
+      <ion-toolbar color="none" >
+        @if(!!logo()){
+        <ion-item lines="none" color="none">
+          <img
+            [src]="logo()"
+          />
+        </ion-item>
+        }
+      </ion-toolbar>
+    </ion-header>
+  `,
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ThisFilmFinderHeaderComponent {
+  logo = input.required<string | null>();
+  name = input.required<string | null>();
+}
